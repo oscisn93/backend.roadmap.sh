29af9134a464cc7bdf990d7b4e7f3ce7140ca728
oscisn93 <41174627+oscisn93@users.noreply.github.com>
2025-01-31 00:14:20 -0800

    Updated flake file

--- task-tracker/flake.nix
+++ task-tracker/flake.nix
@@ -1,9 +1,17 @@
 {
   description = "A nix flake that packages the task-cli command";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+    deno = {
+      type = "github";
+      owner = "denoland";
+      repo = "deno";
+      flake = false;
+    };
   };
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, deno }: {
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
     packages.x86_64-linux.default = with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation {
